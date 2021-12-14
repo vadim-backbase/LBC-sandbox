285abc91637e33612c45b96b7564394abbf95948
Jack Kang <jackk@backbase.com>
2021-12-14 17:50:30 -0500

    Build image from jar using jib plugin

--- account-mock-account-mock-1.38.14/src/main/java/com/backbase/account/mock/MockApplication.java
+++ account-mock-account-mock-1.38.14/src/main/java/com/backbase/account/mock/MockApplication.java
@@ -2,18 +2,11 @@ package com.backbase.account.mock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-public class MockApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MockApplication.class);
-    }
+public class MockApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(MockApplication.class);
